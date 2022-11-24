56790abee88b514d77ce4150a6bb79eea296bb99
NiiHaL <71907716+Nihal182@users.noreply.github.com>
2022-11-24 15:28:25 -0300

    Atividade SA06 SENAI

---
+++ Competencia2/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinamento.Cadastros
+{
+     public class Cliente
+    {
+        //Atributos:
+        public int Codigo;
+        public string Nome;
+
+        //Métodos:
+        public void Mostrar()
+        {
+            Console.WriteLine("Dados do Cliente:{0}--{1}", Codigo, Nome);
+        }
+    }
+
+    class PessoaFisica:Cliente
+    {
+        public string CPF { get; set; }
+
+        public void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine("CPF: {0}", CPF);
+        }
+    }
+
+    class PessoaJuridica : Cliente
+    {
+        public string CNPJ { get; set; }
+
+        public void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine("CNPJ:{0}", CNPJ);
+        }
+    }
+}
---
+++ Competencia2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treinamento.Cadastros;
+
+namespace Competencia2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaFisica pessoa1= new PessoaFisica();
+            pessoa1.Codigo = 123;
+            pessoa1.Nome = "Nayara Arruda";
+            pessoa1.CPF = "04036851179";
+           
+            pessoa1.Mostrar();
+
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++");
+
+            PessoaJuridica pessoa2 = new PessoaJuridica();
+            pessoa2.Codigo = 123;
+            pessoa2.Nome = "Nihal Store";
+            pessoa2.CNPJ = "3453685140001";
+
+            pessoa2.Mostrar();  
+            
+            Console.ReadLine(); 
+
+        }
+    }
+}
